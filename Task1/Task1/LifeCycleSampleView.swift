8cfc8753ce1318269f911529a1dd26d450623c6d
naoki-mrmt <20cm0135@jec.ac.jp>
2022-10-12 20:29:47 +0900

    change file name

---
+++ Task1/Task1/LifeCycleSampleView.swift
@@ -0,0 +1,20 @@
+//
+//  LifeCycleSampleView.swift
+//  Task1
+//
+//  Created by Naoki Muramoto on 2022/10/12.
+//
+
+import SwiftUI
+
+struct LifeCycleSampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LifeCycleSampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        LifeCycleSampleView()
+    }
+}
