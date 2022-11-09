35198f0ae725adf819c164ab1c4771cfcefc282e
naoki-mrmt <20cm0135@jec.ac.jp>
2022-11-09 20:27:07 +0900

    add list sample

---
+++ Task3/Task3/ListSampleView.swift
@@ -0,0 +1,28 @@
+//
+//  ListSampleView.swift
+//  Task3
+//
+//  Created by Naoki Muramoto on 2022/11/09.
+//
+
+import SwiftUI
+
+struct ListSampleView: View {
+    // MARK: - Body
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(AppConst.Text.dummyList, id: \.self) {
+                    Text($0)
+                }
+            } //: List
+            .navigationTitle(AppConst.Text.title)
+        }
+    }
+}
+
+struct ListSampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListSampleView()
+    }
+}
