741e21de694c33467f44f87c7926a7709a213c1b
naoki-mrmt <20cm0135@jec.ac.jp>
2022-11-20 17:04:17 +0900

    delete unnecessary files

--- Scripts/licensePlist.sh
+++
@@ -1,1 +0,0 @@
-${PODS_ROOT}/LicensePlist/license-plist --output-path "$TARGET_NAME/Settings.bundle" --github-token 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
--- Scripts/swiftLint.sh
+++
@@ -1,6 +0,0 @@
-if which swiftlint >/dev/null; then
-  swiftlint --fix
-  swiftlint
-else
-  echo "warning: Swiftlint not installed, download from https://github.com/realm/SwiftLint"
-fi
